e2f5e69b0821f31e56aec6ec6d52fa3f8c1fb7b9
jpothanc <70871841+jpothanc@users.noreply.github.com>
2023-09-04 14:20:39 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: './',
-  }
-
-  if (command !== 'serve') {
-    config.base = './jpothanc.github.io/'
-  }
-
-  return config
+export default defineConfig({
+   plugins: [react()],
+   base: '/jpothanc.github.io/'
 })
