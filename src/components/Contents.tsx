338c62876ccd6e2e69cd16046545664cd9160eb2
jpothanc <70871841+jpothanc@users.noreply.github.com>
2023-10-14 01:41:41 +0800

    build portfolio

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -4,10 +4,25 @@ const Contents = () => {
   return (
     <>
       <div className="content">
-        <header className="headerContent"></header>
-        <section className="sideBar">a</section>
+        <header className="headerContent">
+          <section className="intro">
+            <h1 className="first-heading">
+              Hello <span className="emoji">👋</span>
+            </h1>
+            <p className="text">
+              <span className="u-bold">
+                I'm <span className="u-yellow">Tim</span>
+              </span>
+              , <i>a full-stack developer</i>
+              <span className="text--second">based in the United States.</span>
+            </p>
+          </section>
+        </header>
         <main className="main">
-          <div className="card">a</div>
+          <div className="card">
+            About
+            <button className="btn--download">Download</button>
+          </div>
           <div className="card"></div>
           <div className="card"></div>
           <div className="card"></div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -24,35 +24,17 @@ const NavBar = () => {
     <>
       <header className="header">
         <div className="top-bar">
-          <div className="top-bar__content">
-            <section className="phone">
-              <i className="fa fa-phone icon" aria-hidden="true"></i>
-              1-800922-04444
-            </section>
-            <section className="email">
-              <i className="fa fa-envelope icon" aria-hidden="true"></i>
-              kaljessy@gmail.com
-            </section>
-          </div>
-        </div>
-        <div className="bottom-bar">
           <div className="bottom-bar__content">
             <a href="#" className="logo">
-              <img src="../public/vite.svg" alt="logo" className="logo__img" />
+              <img
+                src="https://images.unsplash.com/photo-1570211776091-c19f426d37af?auto=format&fit=crop&w=70&q=100"
+                alt="logo"
+                className="logo"
+              />
               <span className="logo__text">Jessish Pothancheri</span>
             </a>
             <nav className={navStyle} data-identifier="navStyle">
               <ul className="nav__list">
-                <li className="nav__item">
-                  <a href="#" className="nav__link">
-                    Solutions
-                  </a>
-                </li>
-                <li className="nav__item">
-                  <a href="#" className="nav__link">
-                    Projects
-                  </a>
-                </li>
                 <li className="nav__item">
                   <a href="#" className="btn">
                     Contact
