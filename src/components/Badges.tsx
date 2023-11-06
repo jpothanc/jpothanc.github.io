4956f45f8d5001bfcc378782e9311e3824170cdf
jpothanc <70871841+jpothanc@users.noreply.github.com>
2023-11-06 18:09:45 +0800

    links updation and font size change

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -71,7 +71,7 @@ const Badges = ({ title, icon, content, items: skills, color }: badgeProps) => {
                   //borderRadius: "400px",
                   border: "0px solid #FFEBEE",
                   // fontWeight: 500,
-                  fontSize: "13px",
+                  fontSize: "12px",
                   color: getColor(false),
                 }}
                 key={index}
