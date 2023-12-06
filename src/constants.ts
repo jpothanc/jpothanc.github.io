2bbb3ff45a297304238b91279c2edc38718adce4
jpothanc <70871841+jpothanc@users.noreply.github.com>
2023-12-07 02:27:14 +0800

    mobile friendly changes

--- src/constants.ts
+++ src/constants.ts
@@ -37,7 +37,7 @@ export const fmScaleText = {
 };
 export const fmScaleAboutText = {
   animate: {
-    fontSize: "36px",
+    fontSize: "32px",
     filter: "saturate(200%)",
 
     //textDecoration: "underline",
