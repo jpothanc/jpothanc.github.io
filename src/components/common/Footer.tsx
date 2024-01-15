c185705d6a2b20457da4ee17e6f9a6ca92220a8c
jpothanc <70871841+jpothanc@users.noreply.github.com>
2024-01-15 19:35:05 +0800

    update the counter alignment in footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,14 +2,16 @@ import config from "../../config/config.json";
 import Counter from "../Counter";
 const Footer = () => {
   return (
-    <div className="footer">
-      <p>
-        &copy; 2023 {config.portfolio.firstname}&nbsp;
-        {config.portfolio.secondname}. All Rights Reserved. | Dream, Code,
-        Inspire and Innovate.
-      </p>
+    <>
+      <div className="footer">
+        <p>
+          &copy; 2023 {config.portfolio.firstname}&nbsp;
+          {config.portfolio.secondname}. All Rights Reserved. | Dream, Code,
+          Inspire and Innovate.
+        </p>
+      </div>
       <Counter />
-    </div>
+    </>
   );
 };
 
