f9721ef79868d524b5c5fc418ee071d05ac264db
jpothanc <70871841+jpothanc@users.noreply.github.com>
2024-10-11 10:57:31 +0800

    hover with a cursor pointer

--- src/components/CodeEditor/CodeContent/EditorTab.tsx
+++ src/components/CodeEditor/CodeContent/EditorTab.tsx
@@ -14,7 +14,7 @@ const EditorTab = ({title, icon, selected, onclick }:EditorTabProps) => {
   return (
     <>
       <div className={`flex items-center justify-around h-7 gap-2 pl-1 pr-1 
-      text-xs border  border-zinc-700
+      text-xs border  border-zinc-700 hover:cursor-pointer 
       ${selected ? 'bg-zinc-900 text-white' : 'bg-zinc-800 text-white/50'}
        `}
       onMouseEnter={() => setShowClose(true)}
@@ -29,8 +29,7 @@ const EditorTab = ({title, icon, selected, onclick }:EditorTabProps) => {
         </span>
         {title}
         <span className={`w-4 h-4 flex items-center justify-center ${showClose ? '' : 'invisible'}
-         hover:bg-zinc-700
-        hover:text-white cursor-pointer 
+         hover:bg-zinc-700 
         `}>
           <Icon icon="ic:baseline-close" style={{ fontSize: "1.4rem" }} />
         </span>
